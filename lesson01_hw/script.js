6e3209ba077d98b18be1893f95934bd817df8aca
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-03-31 18:06:22 +0300

    Создана новая ветка

---
+++ lesson01_hw/script.js
@@ -0,0 +1,11 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert("Let's try to understand git /face-palm/");
+
+console.log("let's imagine that you've already done /or that you are cat/");
+
