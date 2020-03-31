e31eb8a02e87f7b0e0dff9cf5f631544d4913b4f
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-03-31 23:22:56 +0300

    modified:   script.js

--- lesson01_hw/script.js
+++ lesson01_hw/script.js
@@ -8,4 +8,27 @@ let period;
 alert("Let's try to understand git /face-palm/");
 console.log("let's imagine that you've already done /or that you are cat/");
 
+money = 200000;
+income = 'project design work';
+addExpenses = 'appartment, sport, transport, clothes';
+deposit = true;
+mission = 4000000;
+period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('The period is ' + period + ' ' + 'months');
+console.log('The goal to earn is ' + 'mission' + ' ' + 'rubles');
+
+arr = addExpenses.toLowerCase().split(', ')
+console.log(arr);
+
+let budgetDay;
+budgetDay = money / 30;
+console.log(budgetDay);
+
+
 
