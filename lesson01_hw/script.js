3ee49b0ce0c2b39126ccb1a420487bab2634194b
lavrTatiana <62764963+lavrTatiana@users.noreply.github.com>
2020-04-01 13:53:01 +0300

    Update script.js
    'HW complicated'

--- lesson01_hw/script.js
+++ lesson01_hw/script.js
@@ -1,34 +1,13 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let num = 266219;
+let result = 1;
+let rest = num;
+let i = num.toString().length;
+for (; i !== 0; i--) {
+  number = Math.trunc(rest / Math.pow(10, (i - 1)));
+  rest -= (number * Math.pow(10, (i - 1)));
+  result *= number;
+}
 
-alert("Let's try to understand git /face-palm/");
-console.log("let's imagine that you've already done /or that you are cat/");
-
-money = 200000;
-income = 'project design work';
-addExpenses = 'appartment, sport, transport, clothes';
-deposit = true;
-mission = 4000000;
-period = 12;
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-console.log(addExpenses.length);
-console.log('The period is ' + period + ' ' + 'months');
-console.log('The goal to earn is ' + 'mission' + ' ' + 'rubles');
-
-arr = addExpenses.toLowerCase().split(', ')
-console.log(arr);
-
-let budgetDay;
-budgetDay = money / 30;
-console.log(budgetDay);
-
+console.log(result);
 
 
