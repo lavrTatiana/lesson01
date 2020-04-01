209541e2e3861fd24f1fd2527e03ffff9b13308a
lavrTatiana <62764963+lavrTatiana@users.noreply.github.com>
2020-04-01 17:07:49 +0300

    Update script.js
    Исправленное задание

--- lesson01_hw/script.js
+++ lesson01_hw/script.js
@@ -1,34 +1,22 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 200000,
+  addExpenses = 'appartment, sport, transport, clothes',
+  income = 'project design work',
+  deposit = true, 
+  mission = 4000000,
+  period = 12,
+  arr = addExpenses.toLowerCase().split(', '),
+  budgetDay = money / 30;
+
 
 alert("Let's try to understand git /face-palm/");
 console.log("let's imagine that you've already done /or that you are cat/");
-
-money = 200000;
-income = 'project design work';
-addExpenses = 'appartment, sport, transport, clothes';
-deposit = true;
-mission = 4000000;
-period = 12;
-
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
-
 console.log(addExpenses.length);
 console.log('The period is ' + period + ' ' + 'months');
 console.log('The goal to earn is ' + 'mission' + ' ' + 'rubles');
-
-arr = addExpenses.toLowerCase().split(', ')
 console.log(arr);
-
-let budgetDay;
-budgetDay = money / 30;
 console.log(budgetDay);
-
 
 
