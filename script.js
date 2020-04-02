1fabbabe25880ffe40dca5b9f5c03c832f09ce54
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-04-02 19:49:20 +0300

    Код предыдущего задания

---
+++ script.js
@@ -0,0 +1,20 @@
+let money = 200000,
+  addExpenses = 'appartment, sport, transport, clothes',
+  income = 'project design work',
+  deposit = true, 
+  mission = 4000000,
+  period = 12,
+  arr = addExpenses.toLowerCase().split(', '),
+  budgetDay = money / 30;
+
+
+alert("Let's try to understand git /face-palm/");
+console.log("let's imagine that you've already done /or that you are cat/");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('The period is ' + period + ' ' + 'months');
+console.log('The goal to earn is ' + 'mission' + ' ' + 'rubles');
+console.log(arr);
+console.log(budgetDay);
