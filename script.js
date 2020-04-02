ea88c4ca574baab4259cca8989ca757624242d4d
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-04-02 22:30:13 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-let money = 200000,
-  addExpenses = 'appartment, sport, transport, clothes',
+let money = +prompt('What is your monthly income?'),
+  addExpenses = prompt('List the possible expenses for the calculated period, separated by commas'),
   income = 'project design work',
-  deposit = true, 
-  mission = 4000000,
-  period = 12,
-  arr = addExpenses.toLowerCase().split(', '),
-  budgetDay = money / 30;
+  deposit = prompt("Do you have a deposit at the bank? Please, write 'yes' or 'no'"), 
+  mission = 400000,
+  arr = addExpenses.toLowerCase().split(','),
+  expenses1 = prompt('Enter a required expense'),
+  amount1 = parseFloat(prompt('How much does it cost?')),
+  expenses2 = prompt('Enter a required expense'),
+  amount2 = parseFloat(prompt('How much does it cost?')), 
+  budgetMonth = (money - amount1 - amount2),
+  budgetDay = budgetMonth / 30,
+  period = Math.ceil(mission / budgetMonth);
+  
 
+// Boolean for deposit
+if (deposit == 'yes') {
+  deposit = true;
+} else {
+  deposit = false; 
+}
 
-alert("Let's try to understand git /face-palm/");
-console.log("let's imagine that you've already done /or that you are cat/");
+// constraction fot budgetDay
+switch (budgetDay) {
+  case (budgetDay >= 1200):
+    console.log('You have high income');
+    break;
+  case ((budgetDay >= 600) && (budgetDay < 1200)):
+    console.log('You have average income');
+    break;
+  case ((budgetDay < 600) && (budgetDay >= 0)):
+    console.log('Unfortunately, your income is below average');
+    break;
+  default:
+    console.log('Something wrong');    
+}
+
+// alert("Let's try to understand git /face-palm/");
+// console.log("let's imagine that you've already done /or that you are cat/");
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('The period is ' + period + ' ' + 'months');
-console.log('The goal to earn is ' + 'mission' + ' ' + 'rubles');
+console.log('The goal to earn is ' + mission + ' ' + 'rubles');
 console.log(arr);
-console.log(budgetDay);
+console.log(Math.floor(budgetDay));
