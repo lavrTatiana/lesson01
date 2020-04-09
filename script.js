7d10fe86d6672ee38f8f7ca313f939b400398b4b
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-04-09 21:39:02 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let money,
       do {
         money = prompt('What is your monthly income?');
       }
-      while (isNan(money) || money === '' || money === null);
+      while (isNaN(money) || money === '' || money === null)
     }
 
+start();
 
 let appData = {
     budget: money,
@@ -14,73 +15,93 @@ let appData = {
     expenses: {},
     addExpenses: [],
     deposit: false,
+    persentDeposit: 0,
+    moneyDeposit: 0,
     mission: 400000,
     period: 3,
     budgetDay: 0,
     budgetMonth: 0,
     expensesMonth: 0,
-    targetMonth: 0,
-
+    
     asking: function() {
+
+      if (confirm('Do you have an extra income?')) {
+        let itemIncome = prompt('What kind of the extra income do you have?');
+        let cashIncome = prompt('How much you earn on this?');
+        appData.income[itemIncome] = cashIncome;
+      }
+
       let addExpenses = prompt('List the possible expenses for the calculated period, separated by commas');
       appData.addExpenses = addExpenses.toLowerCase().split(',');
       appData.deposit = confirm('Do you have a deposit at the bank?');
       
       for (let i = 0; i < 2; i++) {
-        appData.expenses[prompt('Enter a required expense')] =+prompt('How much does it cost?');
+
+        let itemExpenses = prompt('Enter a required expense');
+        let cashExpenses;
+        do {
+          cashExpenses = prompt('How much does it cost?');
+        }
+        while (isNaN(cashExpenses) || cashExpenses === '' || cashExpenses === null);
+        
+        appData.expenses[itemExpenses] = cashExpenses;
       }
     },
 
     getExpensesMonth: function() {
       let sumOfExpenses = 0;  
       for (let key in appData.expenses){
-        sumOfExpenses += appData.expenses[key];
+        appData.expensesMonth += +appData.expenses[key];
       }
-      return sumOfExpenses;
     },
 
-    getBudget: function(arg1, arg2) {
-      return (arg1 - arg2);
+    getBudget: function() {
+      appData.budgetMonth = appData.budget - appData.expensesMonth;
+      appData.budgetDay = Math.floor(appData.budgetMonth / 30);
     },
 
-    getTargetMonth: function(arg1, arg2) {
-      appData.period = Math.ceil(arg1 / arg2);
-      return (appData.period);
+    getTargetMonth: function() {
+      return (Math.ceil(appData.mission / appData.budgetMonth));
     },
     
-    getStatusIncome: function(arg1){
-      if (arg1 >= 1200){
+    getStatusIncome: function(){
+      if (appData.budgetDay >= 1200){
         return ('You have high income');
       }
-      else if (arg1 >= 600 && arg1 < 1200){
+      else if (appData.budgetDay >= 600 && appData.budgetDay < 1200){
         return ('You have average income');
       }
-      else if (arg1 < 600 && arg1 >= 0){
+      else if (appData.budgetDay < 600 && appData.budgetDay >= 0){
         return ('Unfortunately, your income is below average');
       }
       else {
         return ('Something wrong');
       }
+    },
+
+    getInfoDeposit: function() {
+      if (appData.deposit) {
+        appData.persentDeposit = prompt('What is the annual percentage?');
+        appData.moneyDeposit = prompt('How much is deposited in the account?')
+      }
+    },
+
+    calcSavedMoney: function() {
+      return appData.budgetMonth * appData.period;
     }
 }
 
-start();
+
 appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
 
-// Enter the expenses
-appData.expensesMonth = appData.getExpensesMonth();
 
-// Accumulate sum of the expenses
-appData.budgetMonth = appData.getBudget(money, appData.expensesMonth);
-
-// Accumulate budget per day
-appData.budgetDay = appData.budgetMonth / 30;
-
-function respondOnDeadline(targetMonth) {
+function respondOnDeadline() {
   let responseOnDeadline;
-  if (targetMonth > 0 && targetMonth != 1) {
-    responseOnDeadline = ('The mission will be achieved in ' + targetMonth + ' month(s)');
-  } else if (targetMonth === 1) {
+  if (appData.getTargetMonth() > 0 && appData.getTargetMonth() != 1) {
+    responseOnDeadline = ('The mission will be achieved in ' + appData.getTargetMonth() + ' month(s)');
+  } else if (appData.getTargetMonth() === 1) {
     responseOnDeadline = ('The mission will be achieved in this month');
   } else {
     responseOnDeadline = ('Unfortunately, under these conditions, the mission will not be achieved');
@@ -88,10 +109,12 @@ function respondOnDeadline(targetMonth) {
   return (responseOnDeadline);
 }
 
+console.log('Monthly expenses are ' + appData.expensesMonth);
+console.log(respondOnDeadline());
+console.log(appData.getStatusIncome());
+
+
 for (let key in appData) {
-  console.log('This program includes data: ' + key + appData[key]);
+  console.log('This program includes data: ' + key + '-' + appData[key]);
 }
 
-console.log('Monthly expenses are ' + appData.expensesMonth);
-console.log(respondOnDeadline(appData.getTargetMonth(appData.mission, appData.budgetMonth)));
-console.log(appData.getStatusIncome(appData.budgetDay));
