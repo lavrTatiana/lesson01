3907afcccedde7d853375d1a1a6850a299fe4a97
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-04-23 18:09:01 +0300

    All ready!

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ DomElement.prototype.creator = function() {
     elem.style.cssText = `height: ${this.height}; 
                           width: ${this.width}; 
                           background: ${this.bg}; 
-                          fontSize: ${this.fontSize}`;
+                          font-size: ${this.fontSize}`;
     document.body.append(elem);
     console.log(elem);
     
@@ -32,7 +32,7 @@ DomElement.prototype.creator = function() {
     elem2.style.cssText = `height: ${this.height}; 
                           width: ${this.width}; 
                           background: ${this.bg}; 
-                          fontSize: ${this.fontSize}`;
+                          font-size: ${this.fontSize}`;
     document.body.append(elem2);
     console.log(elem2);
     
@@ -42,15 +42,15 @@ DomElement.prototype.creator = function() {
 
 
 
-const element1 = new DomElement({selector: '.dom', height: '100', width: '90', bg: 'green', fontSize: '20px'});
+const element1 = new DomElement({selector: '.dom', height: '100px', width: '300px', bg: 'green', fontSize: '30px'});
 element1.creator();
 document.getElementsByClassName('dom')[0].innerText = 'We would be young forever';
 
 
-const element2 = new DomElement({selector: '.bom', height: '50', width: '90', bg: 'red', fontSize: '50px'});
+const element2 = new DomElement({selector: '.bom', height: '50px', width: '900px', bg: 'red', fontSize: '40px'});
 element2.creator();
 document.getElementsByClassName('bom')[0].innerText = 'We would be young forever';
 
-const element3 = new DomElement({selector:'#for', height: '40', width: '80', bg: '#3333', fontSize: '8'});
+const element3 = new DomElement({selector:'#for', height: '40px', width: '80px', bg: '#3333', fontSize: '8px'});
 element3.creator();
 document.getElementById('for').innerText = 'We would be young forever';
